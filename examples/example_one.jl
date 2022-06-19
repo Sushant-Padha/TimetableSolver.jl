66ac038120726de1355ab29100d53edfa7057a57
sushant <sushant.padha@gmail.com>
2022-06-19 20:27:28 +0530

    chore: :sparkles: add commented code for printing model constraints with name
    + update `precompile_execution.jl` with pseudo-anonymized names for example (from `examples\example_one.jl`)

--- examples/example_one.jl
+++ examples/example_one.jl
@@ -3,33 +3,28 @@ using TimetableSolver
 C9 = Division(9, 3)
 D10 = Division(10, 4)
 
-subjectcounts_C9 = SubjectCounts(
-    "MATH"=>2, "ENG"=>2, "SPORTS"=>1
-)
-subjectcounts_D10 = SubjectCounts(
-    "MATH"=>2, "ENG"=>1, "SCIENCE"=>2
-)
+subjectcounts_C9 = SubjectCounts("MATH" => 2, "ENG" => 2, "SPORTS" => 1)
+subjectcounts_D10 = SubjectCounts("MATH" => 2, "ENG" => 1, "SCIENCE" => 2)
 
-mark = Teacher("Mark Andrews",  "MARK",     ["MATH","SCIENCE"], [9, 10])
-john = Teacher("John Curry",    "JOHN",     ["MATH"],           [9])
-geeta = Teacher("Geeta Gupta",  "GGA",      ["ENG"],            [9, 10])
-paul = Teacher("Paul Reid",     "PAUL",     ["SPORTS"],         [9])
-jane = Teacher("Jane Murphy",   "JANE",     ["SCIENCE"],        [10])
+mark = Teacher("Mark Andrews", "MARK", ["MATH", "SCIENCE"], [9, 10])
+john = Teacher("John Curry", "JOHN", ["MATH"], [9])
+geeta = Teacher("Geeta Gupta", "GGA", ["ENG"], [9, 10])
+paul = Teacher("Paul Reid", "PAUL", ["SPORTS"], [9])
+jane = Teacher("Jane Murphy", "JANE", ["SCIENCE"], [10])
 
-tt_C9 = Timetable(
-    [3, 2],
-    subjectcounts_C9,
-    [mark, john, geeta, paul],
-    C9
-)
-tt_D10 = Timetable(
-    [3, 2],
-    subjectcounts_D10,
-    [mark, geeta, jane],
-    D10
-)
+tt_C9 = Timetable([3, 2], subjectcounts_C9, [mark, john, geeta, paul], C9)
+tt_D10 = Timetable([3, 2], subjectcounts_D10, [mark, geeta, jane], D10)
 
 schedule = Schedule(tt_C9, tt_D10)
 
 @time solution, status = solve!(schedule)
+#= print all constraints with names
+
+vardata = VariableData(schedule)
+model, modelvars = get_model(vardata)
+for (f,s) in list_of_constraint_types(model)
+    display(all_constraints(model, f, s))
+end
+
+=#
 schedule
--- precompile_execution.jl
+++ precompile_execution.jl
@@ -5,31 +5,17 @@ using TimetableSolver
 C9 = Division(9, 3)
 D10 = Division(10, 4)
 
-ajay = Teacher("Ajay Magotra", "AJA", ["MATH",], [9, 10])
-kotwal = Teacher("Kotwal Singh", "KOT", ["MATH",], [9,])
-deepika = Teacher("Deepika Ibikunle", "DPK", ["ENG",], [9, 10])
-ganguly = Teacher("Ganguly A", "GNG", ["SPORTS",], [9,])
-sanjay = Teacher("Sanjay Pandey", "SNJ", ["SCIENCE",], [10,])
+subjectcounts_C9 = SubjectCounts("MATH" => 2, "ENG" => 2, "SPORTS" => 1)
+subjectcounts_D10 = SubjectCounts("MATH" => 2, "ENG" => 1, "SCIENCE" => 2)
 
-subjectcounts_C9 = OrderedDict(
-    "MATH"=>2, "ENG"=>2, "SPORTS"=>1
-)
-subjectcounts_D10 = OrderedDict(
-    "MATH"=>2, "ENG"=>1, "SCIENCE"=>2
-)
+mark = Teacher("Mark Andrews", "MARK", ["MATH", "SCIENCE"], [9, 10])
+john = Teacher("John Curry", "JOHN", ["MATH"], [9])
+geeta = Teacher("Geeta Gupta", "GGA", ["ENG"], [9, 10])
+paul = Teacher("Paul Reid", "PAUL", ["SPORTS"], [9])
+jane = Teacher("Jane Murphy", "JANE", ["SCIENCE"], [10])
 
-tt_C9 = Timetable(
-    [3, 2],
-    subjectcounts_C9,
-    [ajay, kotwal, deepika, ganguly],
-    C9
-)
-tt_D10 = Timetable(
-    [3, 2],
-    subjectcounts_D10,
-    [ajay, deepika, sanjay],
-    D10
-)
+tt_C9 = Timetable([3, 2], subjectcounts_C9, [mark, john, geeta, paul], C9)
+tt_D10 = Timetable([3, 2], subjectcounts_D10, [mark, geeta, jane], D10)
 
 schedule = Schedule(tt_C9, tt_D10)
 
@@ -42,3 +28,13 @@ rawsolution, status = get_solution(model, modelvars, vardata)
 solution = convertsolution(rawsolution, vardata)
 applysolution!(schedule, solution)
 solution, status = solve!(schedule)
+
+#= print all constraints with names
+
+vardata = VariableData(schedule)
+model, modelvars = get_model(vardata)
+for (f,s) in list_of_constraint_types(model)
+    display(all_constraints(model, f, s))
+end
+
+=#
